f1ad063bcd8e38de4d8f65c90488637a2185c614
Suhana Ayisha <31471461+suhanaayisha@users.noreply.github.com>
2026-01-17 15:14:45 +0400

    automate workflow

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
+import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
 
-app.get("/api/name", (req: Request, res: Response) => {
-  res.json({ name: "Suhana Ayisha" });
+app.get('/api/name', (req: Request, res: Response) => {
+  res.json({ name: 'Suhana Ayishaa' });
+});
+
+app.get('/api/date', (req: Request, res: Response) => {
+  res.json({ date: new Date() });
 });
 
 export default app;
